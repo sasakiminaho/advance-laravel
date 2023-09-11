9a4a1e1ba963f7fb7e8e9a79e355120aa9f30e37
sasaki minaho <m37h0kl.u@gmail.com>
2023-09-11 12:37:41 +0900

    first commit

---
+++ src/resources/views/author/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.default')
+<style>
+th {
+  background-color: #289ADC;
+  color: white;
+  padding: 5px 40px;
+}
+
+tr:nth-child(odd) td {
+  background-color: #FFFFFF;
+}
+
+td {
+  padding: 25px 40px;
+  background-color: #EEEEEE;
+  text-align: center;
+}
+</style>
+@section('title', 'author.index.blade.php')
+
+@section('content')
+<table>
+  <tr>
+    <th>Author</th>
+    <th>Book</th>
+  </tr>
+  @foreach ($items as $item)
+  <tr>
+    <td>
+      {{$item->getDetail()}}
+    </td>
+    <td>
+        @if ($item->books != null)
+        <table width="100%">
+            @foreach ($item->books as $obj)
+            <tr>
+                <td>{{ $obj->getTitle() }}</td>
+            </tr>
+            @endforeach
+        </table>
+        @endif
+  </tr>
+  @endforeach
+</table>
+@endsection
